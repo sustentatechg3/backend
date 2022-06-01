2f11b591b390e3d8cbb727f7ad7b8670c42e5690
unknown <andre.dev.oliveira@gmail.com>
2022-06-01 09:43:36 -0300

    Projeto integrador tk3

---
+++ src/main/java/com/generation/sustentatech/SustentatechApplication.java
@@ -0,0 +1,13 @@
+package com.generation.sustentatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustentatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustentatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/sustentatech/SustentatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.sustentatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustentatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
