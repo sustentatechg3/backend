9d1a2c85db7fe59274f450032fee71dfa73ff69d
jonathanhenriques <silva.henriquejonas@gmail.com>
2022-06-01 10:46:13 -0300

    Criando pacote Model e Subindo classe categoria mapeada

---
+++ src/main/java/com/generation/sustentatech/model/Categoria.java
@@ -0,0 +1,41 @@
+package com.generation.sustentatech.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo Tipo não pode ser vazio e nem ser um espaço em branco!")
+	@Size(min = 5, max = 255, message = "No mínimo 5 e máximo 255 caracteres!")
+	private String tipo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+
+	
+}
