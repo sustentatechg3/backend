0f6e09648583310c219fb85ec4a03c81960c95d4
unknown <andre.dev.oliveira@gmail.com>
2022-06-07 11:46:08 -0300

    criação da classe model de produtos e usuarios

---
+++ src/main/java/com/generation/sustentatech/model/Produtos.java
@@ -0,0 +1,94 @@
+package com.generation.sustentatech.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produtos {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+
+	@Size(min = 1, max = 255, message = "Seu nome deve ter no minimo 3 caracter")
+	private String descricao;
+	
+	@Digits(integer = 3, fraction = 2, message = "Apenas centenas e 2 casas após o ponto.")
+	@Positive
+	@NotBlank(message = "O valor é obrigatorio")
+	private BigDecimal valor;
+	
+	@NotBlank(message = "O nome do Produto é obrigatorio")
+	@Size(min = 3, max = 100, message = "Seu produto deve ter no minimo 3 caracter")
+	private String nome;
+	
+	private String foto;
+	
+	@ManyToOne
+	@JsonIgnoreProperties
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+
+}
