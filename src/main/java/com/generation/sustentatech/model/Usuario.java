c31fdf93946e9225bbc681026d06e6c927726da2
CarolBSantos <carolinabsantos23@gmail.com>
2022-06-15 11:00:01 -0300

    Deploy Projeto Integrador

--- src/main/java/com/generation/sustentatech/model/Usuario.java
+++ src/main/java/com/generation/sustentatech/model/Usuario.java
@@ -14,6 +14,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
@@ -26,6 +28,7 @@ public class Usuario {
 	@Size(min = 3, max = 100, message = "Seu nome deve ter no minimo 3 caracter")
 	private String nome;
 	
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O email é obrigatorio")
 	@Email
 	private String usuario;
